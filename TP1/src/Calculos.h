7d06874aaa390759c67eb55e3ba916168ee27e6d
TomasMastra <78211532+TomasMastra@users.noreply.github.com>
2021-09-14 20:52:42 -0300

    Add files via upload

---
+++ TP1/src/Calculos.h
@@ -0,0 +1,27 @@
+/*
+ * calculos.h
+ *
+ *  Created on: 9 abr. 2021
+ *      Author: Compumar
+ */
+
+#ifndef CALCULOS_H_
+#define CALCULOS_H_
+#include <stdio.h>
+
+int RealizarOperaciones(int);
+int Sumar(int, int, int);
+int Restar(int, int, int);
+float Dividir(float, int, int);
+int Multiplicar(int, int, int);
+int FactorialPrimerOperando(int, int);
+int FactorialSegundoOperando(int, int);
+void MostrarEcuaciones(int, int, int, int, int, float, int, int);
+
+
+
+
+
+
+
+#endif /* CALCULOS_H_ */
