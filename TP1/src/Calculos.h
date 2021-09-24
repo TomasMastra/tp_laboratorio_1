8c45e6632adfa65f542b78dd0a5202e51566e55c
TomasMastra <78211532+TomasMastra@users.noreply.github.com>
2021-09-24 18:30:27 +0200

    Add files via upload

---
+++ TP1/src/Calculos.h
@@ -0,0 +1,25 @@
+/*
+ * Calculos.h
+ *
+ *Aca estan las funciones que voy a usar
+ *Los valores los paso por referencia al main
+ *
+ */
+
+#ifndef CALCULOS_H_
+#define CALCULOS_H_
+#include <stdio.h>
+
+int Sumar(int, int, int);
+int Restar(int, int, int);
+float Dividir(float, int, int);
+int Multiplicar(int, int, int);
+long FactorialPrimerOperando(long, int);
+long FactorialSegundoOperando(long, int);
+void MostrarEcuaciones(int, int, int, long, long, float, int, int);
+
+
+
+
+
+#endif /* CALCULOS_H_ */
---
+++ TP1/src/TP1.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include "calculos.h"
+#include "Menu.h"
+
+/*
+
+int PedirNumero1(int);
+int PedirNumero2(int);    */
+
+
+int main() //Mastrapasqua Tomas
+{
+
+    setbuf(stdout, NULL);
+    int opcion=0;
+
+
+    PedirOpcionMenu(opcion);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+ }
+
+/////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
